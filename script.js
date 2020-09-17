08e3bca09b1e895668bcfa21ad970c8bb22e631b
Feda Zidan <fido311@gmail.com>
2020-09-16 23:36:08 -0500

    Finishing the portfolio, added all the links and changed the about paragraph

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 //  Mobile menu // 
-var navLink = document.querySelectorAll('.nav__link')
+var navLink = document.querySelectorAll('.nav_link')
 
 function linkAction(){
     var navMenu = document.getElementById('nav-menu')
@@ -33,9 +33,9 @@ function scrollActive(){
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
         }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
 }
@@ -49,30 +49,30 @@ var sr = ScrollReveal({
 })
 
 // The home section // 
-sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'right', delay: 400})
+sr.reveal('.home_title', {})
+sr.reveal('.home_scroll', {delay: 200})
+sr.reveal('.home_img', {origin:'right', delay: 400})
 
 // About section // 
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+sr.reveal('.about_img', {delay: 500})
+sr.reveal('.about_subtitle', {delay: 300})
+sr.reveal('.about_profession', {delay: 400})
+sr.reveal('.about_text', {delay: 500})
+sr.reveal('.about_social-icon', {delay: 600, interval: 200})
 
 // Skills section // 
-sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills_subtitle', {})
+sr.reveal('.skills_name', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('.skills_img', {delay: 400})
 
 // Portfolio section// 
-sr.reveal('.portfolio__img', {interval: 200})
+sr.reveal('.portfolio_img', {interval: 200})
 
 // Contact section// 
-sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {interval: 200})
-sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
+sr.reveal('.contact_subtitle', {})
+sr.reveal('.contact_text', {interval: 200})
+sr.reveal('.contact_input', {delay: 400})
+sr.reveal('.contact_button', {delay: 600})
 
 
 
